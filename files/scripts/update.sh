c0fbae2e9530aea70d325fbd41cdbde31e16e2b1
HappyFrowl <150538444+HappyFrowl@users.noreply.github.com>
2024-03-22 21:56:23 +0100

    yml fix

--- files/scripts/update.sh
+++ files/scripts/update.sh
@@ -4,11 +4,41 @@
 
 set -e
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
+echo "Welcome to Update Manager"
+echo ""
 
-sudo flatpak update
+update_system() {
+	echo "Starting system update"
+	sleep 1
+	echo "Please provide password"
+	echo ""
+
+	sudo apt update
+	echo ""
+
+	sudo apt upgrade -y
+	echo ""
+
+	sudo apt dist-upgrade -y
+	echo ""
+
+	sudo apt autoremove -y
+	echo ""
+}
+
+error_handling() {
+	echo "An error has occurred: $1"
+	exit 1
+}
+
+trap 'error_handling "Line $LINENO: Command failed with exit code $?"' ERR
+
+update_system || error_handling "issues encountered" 
+
+echo "Starting with FlatPak updates"
+
+sudo flatpak update -y
+sudo flatpak uninstall --unused
+
 
 exit
