d62e7ded03f72ebfe7e32d95cc99481841263df6
HappyFrowl <150538444+HappyFrowl@users.noreply.github.com>
2024-06-01 17:39:49 +0200

    make nicer

--- files/scripts/remove-unusedkernels.sh
+++ files/scripts/remove-unusedkernels.sh
@@ -8,40 +8,39 @@ DEB=$(uname -r)
 echo "In use kernel version: $IN_USE"
 echo "Debian release: $DEB"
 
-#Old kernels
 OLD_KERNELS=$(
 	dpkg --list |
 		grep -Ei 'linux-headers-|linux-image-' |
 		grep -v $IN_USE |
 		awk '{print $2}'
 )
-
 echo -e "\nOld kernels present:"
 echo $OLD_KERNELS
 
-#Old modules
-OLD_MODULES=$(ls /lib/modules |
-	grep -v $DEB || true
-)
+OLD_MODULES=$(ls /lib/modules |	grep -v $DEB || true)
 echo "Old modules present:"
 echo "$OLD_MODULES"
 
-#Dry run without exec
+#Dry run when used without exec
 if [ "$1" == "exec" ]; then
 	if [ -n "$OLD_KERNELS" ]; then
 		for each in $OLD_KERNELS; do
 			yes | sudo apt purge "$each"
+			echo "Removed $each"
 		done
 	else
 		echo "There are no unused kernels, skipping..."
 	fi
 
-#Remove unused modules
-	for module in $OLD_MODULES; do
-		sudo rm -rf /lib/modules/$module
-		echo "Removed $module"
-	done
+	if [ -n "$OLD_MODULES" ]; then
+		for each in $OLD_MODULES; do
+			sudo rm -rf /lib/modules/$module
+			echo "Removed $each"
+		done
+	else
+		echo "There are no unused modules, skipping..."
+	fi
 else
-	echo -e "\nTo remove, run as: $0 exec"
+	echo "To remove, run: $0 exec"
 fi
 exit
