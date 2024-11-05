f1e618474ac04825943e21e9042bd2b9cbe703bc
HappyFrowl <150538444+HappyFrowl@users.noreply.github.com>
2024-11-06 00:00:33 +0100

    fix bug

--- files/scripts/remove-unusedkernels.sh
+++ files/scripts/remove-unusedkernels.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #Script for removing unused kernels and modules
 set -e
+IFS=$'\n'
 
 #Kernel in use
 IN_USE=$(uname -v | awk '{print $5}')
 DEB=$(uname -r)
 echo "In use kernel version: $IN_USE"
-echo "Debian release: $DEB"
+echo "Debian release: ${DEB}"
 
 OLD_KERNELS=$(
 	dpkg --list |
@@ -15,27 +16,29 @@ OLD_KERNELS=$(
 		awk '{print $2}'
 )
 echo -e "\nOld kernels present:"
-echo $OLD_KERNELS
+echo "${OLD_KERNELS}"
 
 OLD_MODULES=$(ls /lib/modules |	grep -v $DEB || true)
 echo "Old modules present:"
-echo "$OLD_MODULES"
+echo "${OLD_MODULES}"
 
 #Dry run when used without exec
 if [ "$1" == "exec" ]; then
-	if [ -n "$OLD_KERNELS" ]; then
-		for each in $OLD_KERNELS; do
-			yes | sudo apt purge "$each"
-			echo "Removed $each"
-		done
+	if [ -n "${OLD_KERNELS}" ]; then
+	    IFS=$'\n'
+	    for each in ${OLD_KERNELS}; do
+	        yes | sudo apt purge "$each"
+	        echo "Removed $each"
+	    done
+	    unset IFS
 	else
-		echo "There are no unused kernels, skipping..."
+	    echo "There are no unused kernels, skipping..."
 	fi
 
-	if [ -n "$OLD_MODULES" ]; then
+	if [ -n "${OLD_MODULES}" ]; then
 		for each in $OLD_MODULES; do
 			sudo rm -rf /lib/modules/$each
-			echo "Removed $each"
+			echo "Removed ${each}"
 		done
 	else
 		echo "There are no unused modules, skipping..."
