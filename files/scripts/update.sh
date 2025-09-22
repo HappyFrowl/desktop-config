261f14cd0ff04879e023de3eec8b51cf56c07e8c
HappyFrowl <filipnkalas@gmail.com>
2025-09-23 00:24:28 +0200

    add handle_error function

--- files/scripts/update.sh
+++ files/scripts/update.sh
@@ -2,30 +2,39 @@
 # System update script
 set -e
 
-# Functions:
+#region Function Definitions:
+handle_error(){
+	# Function handling errors
+	echo -e "\e[31mError: $1"
+	echo -e "\e]31mAborting...\e[0m"
+	exit 1
+}
+
+
 
 update() {
 	echo -e "Please provide password\n"
-	sudo apt update
-	echo -e  "\nApt packages:" | tee -a ${LOGFILE}
-	sudo apt dist-upgrade -y 2>>${ERRORLOG} | tee -a ${LOGFILE}
+	sudo apt update || handle_error "Failed to update packages" 
+	echo -e  "\nApt packages:" | tee -a ${LOGFILE} || 
+	sudo apt dist-upgrade -y | tee -a ${LOGFILE} || handle_error "Failed to upgrade packages" 
 }
 
 cleanup() {
-	sudo apt autoremove -yy 2>>${ERRORLOG} | tee -a ${LOGFILE}
-	sudo apt autoclean 2>>${ERRORLOG} | tee -a ${LOGFILE}
+	sudo apt autoremove -yy 2>>${ERRORLOG} | tee -a ${LOGFILE} || handle_error "Failed to autoremove packages"
+	sudo apt autoclean 2>>${ERRORLOG} | tee -a ${LOGFILE} || handle_error "Failed to autoclean" 
 }
 
 update_flatpak() {
-	echo -e "\nFlatPak updates:" | tee -a ${LOGFILE}
-	flatpak update -y  | tee -a ${LOGFILE}
+	echo -e "\nFlatPak updates:" | tee -a ${LOGFILE}  
+	flatpak update -y  | tee -a ${LOGFILE} || handle_error "Failed to update Flatpak" 
 }
 
 cleanup_flatpak() {
-	flatpak uninstall --unused -y | tee -a  ${LOGFILE}
+	flatpak uninstall --unused -y | tee -a ${LOGFILE} || handle_error "Failed to uninstall Flatpak"
 }
 
-# Execution
+#endregion 
+#region init 
 
 # Set the environment variable to noninteractive
 export DEBIAN_FRONTEND="noninteractive"
@@ -38,14 +47,15 @@ ERRORLOG="/var/log/update/error.log"
 FILES=("$LOGFILE" "$ERRORLOG")
 for file in "${FILES[@]}"; do
 	DIR=$(dirname $file)
-	sudo mkdir -p $DIR
+	sudo mkdir -p $DIR || handle_error "Failed to create directory $DIR"
 
 	if [ ! -f $file ]; then
-		sudo touch $FILE
+		sudo touch $FILE || handle_error "Failed to create file $FILE"
 	fi
 done
+#endregion init
 
-# Execute functions
+#region Function Execution
 echo "Welcome to Update Manager"
 echo -e "\n$(date)" | tee -a $LOGFILE
 START_TIME=$SECONDS
@@ -64,5 +74,5 @@ echo "----------------------------------------"
 echo -e "\nTime taken to run updates:"
 ELPASED_TIME=$((${SECONDS} - ${START_TIME}))
 echo "${ELPASED_TIME} seconds"
-
-exit
+#endregion Function Execution
+exit 0
